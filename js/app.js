f0ec0b671807db252c1ca31536fb03ec8440dbab
jordinhuang <jordinhuang@gmail.com>
2022-06-08 12:18:59 -0400

    remove resume and original works display

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
-import {projectData} from "./project-data"
+import {projectData} from "./project-data.js"
 
 const cardContainer = document.getElementById('card-container')
+
+
+let projectMarkup = projectData.map(project =>
+`
+<div class="card text-center" style="width: 18rem;">
+    <img class="card-img-top" src="${project.image}" alt="Card image cap">
+    <div class="card-body">
+        <h5 class="card-title">'${project.title}'</h5>
+        <p class="card-text">'${project.description}</p>
+        <div>
+            <a href="${project.github}" class="btn btn-primary">Github</a>
+            <a href="${project.deployment}" class="btn btn-secondary">Deployment</a>
+        </div>
+    </div>
+</div>
+`
+    ).join('')
+
+cardContainer.innerHTML = projectMarkup
--- js/project-data.js
+++ js/project-data.js
@@ -5,13 +5,10 @@ let projectData = [
         image: './assets/imgs/tic-tac-toe.png',
         github:'https://github.com/jor-din/ttt-weekend',
         description: 'A Tic-Tac-Toe broswer game built with CSS, HTML, JavaScript'
-    },
-    {
-        title: 'Tic-Tac-Toe' , 
-        deployment:'https://app.netlify.com/sites/ttt-jordinh/overview',
-        image: './assets/imgs/tic-tac-toe.png',
-        github:'https://github.com/jor-din/ttt-weekend',
-        description: 'A Tic-Tac-Toe broswer game built with CSS, HTML, JavaScript'
     }
-
 ]
+
+
+export {
+    projectData
+}
