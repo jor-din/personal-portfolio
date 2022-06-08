ee454f081e979de95695faa24e29a1bf2d768e1d
jordinhuang <jordinhuang@gmail.com>
2022-06-08 12:28:34 -0400

    remove img folder

--- js/project-data.js
+++ js/project-data.js
@@ -2,7 +2,7 @@ let projectData = [
     {
         title: 'Tic-Tac-Toe' , 
         deployment:'https://app.netlify.com/sites/ttt-jordinh/overview',
-        image: './assets/imgs/tic-tac-toe.png',
+        image: '/assets/tic-tac-toe.png',
         github:'https://github.com/jor-din/ttt-weekend',
         description: 'A Tic-Tac-Toe broswer game built with CSS, HTML, JavaScript'
     }
