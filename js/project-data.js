9d94d6a151efb166635c222f5770422431105ad1
jordinhuang <jordinhuang@gmail.com>
2022-07-01 04:31:18 -0400

    add new project information

--- js/project-data.js
+++ js/project-data.js
@@ -1,10 +1,17 @@
 let projectData = [
     {
         title: 'Tic-Tac-Toe' , 
-        deployment:'https://app.netlify.com/sites/ttt-jordinh/overview',
+        deployment:'https://ttt-jordinh.netlify.app/',
         image: '/assets/tic-tac-toe.png',
         github:'https://github.com/jor-din/ttt-weekend',
         description: 'A Tic-Tac-Toe broswer game built with CSS, HTML, JavaScript'
+    },
+    {
+        title: 'White Lights' , 
+        deployment:'https://whitelights.herokuapp.com/',
+        image: '/assets/wl-landing page.png',
+        github:'https://github.com/jor-din/powerlift-tracker',
+        description: 'A workout tracker built with MEN-stack'
     }
 ]
 
